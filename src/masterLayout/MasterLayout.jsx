4e9725469dfbcaa627ea9457da22f11534e2842f
haris arif <harif3496@gmail.com>
2025-10-01 12:04:19 +0500

    link updated

--- src/masterLayout/MasterLayout.jsx
+++ src/masterLayout/MasterLayout.jsx
@@ -347,7 +347,7 @@ const MasterLayout = ({ children }) => {
                 </div>
                 <div className="overflow-auto scrollbar">
                   <ul className="nav d-flex flex-column mb-2 pb-1">
-                    <li className="nav-item"><Link className="nav-link px-3 d-block" to="/profile-settings"> <span className="me-2 align-bottom" data-feather="user"></span><span>Profile</span></Link></li>
+                    <li className="nav-item"><Link className="nav-link px-3 d-block" to="/profile"> <span className="" data-feather="user"></span><span>Profile</span></Link></li>
                     <li className="nav-item"><Link className="nav-link px-3 d-block" to="/"><span className="me-2 align-bottom" data-feather="pie-chart"></span>Dashboard</Link></li>
                     <li className="nav-item"><Link className="nav-link px-3 d-block" to="#"> <span className="me-2 align-bottom" data-feather="settings"></span>Settings &amp; Privacy </Link></li>
                     <li className="nav-item"><Link className="nav-link px-3 d-block" to="#"> <span className="me-2 align-bottom" data-feather="help-circle"></span>Help Center</Link></li>
