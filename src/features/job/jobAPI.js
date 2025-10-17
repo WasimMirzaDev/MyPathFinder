a921b240f733090087845d2447e80c3443532b0b
Ahmad Mujtaba <ahmadmujtabap70@gmail.com>
2025-10-17 17:57:22 +0500

    new changes

--- src/features/job/jobAPI.js
+++ src/features/job/jobAPI.js
@@ -3,7 +3,7 @@ import axios from "../../api/axios";
 export const fetchUserJobs = async (formData) => {
   const response = await axios.get(`/fetch-jobs?q=${encodeURIComponent(
     formData.searchQuery + " job"
-        )}&gl=${formData.country}&location=${encodeURIComponent(formData.location)}`); // e.g. /me endpoint
+        )}&gl=${formData.country}&location=${encodeURIComponent(formData.location)}&remote=${formData.remote}`); // e.g. /me endpoint
 //   console.log("request hit",response.data);
   return response.data;
 };
